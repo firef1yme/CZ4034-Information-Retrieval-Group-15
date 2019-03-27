2cdb7ca3f077ce01ff4f16bab6a8a25f7c88cb6b
Emil Luusua <emilluusua@gmail.com>
2019-03-27 18:17:12 +0800

    Removed casefolding

--- process_pan_data.py
+++ process_pan_data.py
@@ -27,7 +27,7 @@ def main():
         labels = user_labels[root.attrib['id']]
         
         for child in root:
-            tweet = child.text[:-2].casefold() # Casefold and ignore last two tabs
+            tweet = child.text[:-2] # Ignore last two tabs
             entry = {'tweet': tweet,
                     'gender': labels[0],
                     'age_group': labels[1],
